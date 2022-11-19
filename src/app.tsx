7fdbc23f88f1173c381e2f408d74739078f7a65f
MikenasIgnas <mikenasignas@gmail.com>
2022-11-19 21:56:35 +0200

    Template

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react';
+import Title from './components/Title';
+
+const App = () => {
+  console.log('Works');
+  return (
+    <Title />
+  );
+};
+export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Title = () => (
+  <div>All Good</div>
+  );
+
+export default Title;
